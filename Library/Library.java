8bb554bc4048fca6ba4d10d3e4f28463e9049f0e
Jian Hong Song <jianhongsong@trentu.ca>
2024-12-01 11:16:54 -0500

    Task2_Question4

--- Library/Library.java
+++ Library/Library.java
@@ -1,22 +1,38 @@
 package Assignement3;
 
+import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
+
+import Assignment3.Book;
 
 public class Library {
     private List<Member> members = new ArrayList<Member>();
     private List<Book> books = new ArrayList<Book>();
 
-    // Add a new member to the library
-    public void addMember(Member member) {
-        members.add(member);
+    // Add a new member to the library. Change method to boolean.
+    public boolean addMember(Member member) {
+        Member memberFind = findMemberById(member.getId());
+        if(memberFind == null){
+        	members.add(member);
+        	return true;//If member not existed in list, add member and return true.
+        	}
+        else{
+        	return false;
+        	}
     }
     
-    // Add a new book to the library
-    public void addBook(Book book) {
-        books.add(book);
+    // Add a new book to the library. Change method to boolean
+    public boolean addBook(Book book) {
+    	Book bookFind = findBookById(book.getId());
+    	if(bookFind == null) {
+    		books.add(book);
+    		return true;//if book doesn't exist in the list, add book and return true.
+    	}
+    	else {
+    		return false;
+    	}
     }
-
     // Find a member by ID
     public Member findMemberById(int id) {
         for (Member member : members) {
--- Library/LibraryManagement.java
+++ Library/LibraryManagement.java
@@ -1,6 +1,9 @@
 package Assignement3;
 
+import java.lang.reflect.Member;
 import java.util.Scanner;
+
+import Assignment3.Book;
 
 public class LibraryManagement {
     private Library library = new Library();
@@ -39,8 +42,12 @@ public class LibraryManagement {
                     scanner.nextLine();
 
                     Member newMember = new Member(id, name);
-                    library.addMember(newMember);
+                    if(library.addMember(newMember)) {
                     System.out.println("Member added successfully.");
+                    }
+                    else {
+                    	System.out.println("Fail to add member due to member " + newMember.getId() + " existed. ");
+                    }
                     break;
                 case 2:
                     System.out.print("Enter book ID: ");
@@ -51,8 +58,12 @@ public class LibraryManagement {
                     scanner.nextLine();
 
                     Book newBook = new Book(id, title);
-                    library.addBook(newBook);
-                    System.out.println("Book added to library successfully.");
+                    if(library.addBook(newBook)) {
+                    	System.out.println("Book added to library successfully.");
+                    }
+                    else {
+                    	System.out.println("Fail to add Book due to book " + newBook.getId() + " " + newBook.getTitle() + " existed. ");
+                    }
                     break;
                 case 3:
                 	System.out.println("\n--- Available Members ---");
