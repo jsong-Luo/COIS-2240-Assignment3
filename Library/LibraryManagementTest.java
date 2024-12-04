dab8f2f7e7e5f0ab6f4d71505a5e2a8f2e4f0f69
Jian Hong Song <jianhongsong@trentu.ca>
2024-12-04 01:03:00 -0500

    Task3_Question2
    Some correction of Question1 also.

--- Library/LibraryManagementTest.java
+++ Library/LibraryManagementTest.java
@@ -2,7 +2,12 @@ package Assignement3;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Member;
+
 import org.junit.Test;
+
+import Assignement3.Transaction;
+import Assignment3.Book;
 
 public class LibraryManagementTest {
 
@@ -13,8 +18,7 @@ public class LibraryManagementTest {
 		Book newBook1 = new Book(100, "Programming");
 		Book newBook2 = new Book(210, "Test2");
 		Book newBook3 = new Book(999, "Test3");
-		Book newBook4 = new Book(1000, "Test4");
-		Book newBook5 = new Book(50, "Test5");
+        Book newBook4 = new Book(100, "Test4");
 		
         // Assertions to validate the results
         assertEquals(100, newBook1.getId());
@@ -26,11 +30,47 @@ public class LibraryManagementTest {
         assertEquals(999, newBook3.getId());
         assertEquals("Test3", newBook3.getTitle());
         
-        assertEquals(1000, newBook4.getId());
-        assertEquals("Test4", newBook4.getTitle());
+        assertTrue(newBook4.isValidId(100));
+        
+        assertFalse(newBook4.isValidId(1000));
+        //assertEquals("Test4", newBook4.getTitle());
        
-        assertEquals(50, newBook5.getId());
-        assertEquals("Test5", newBook5.getTitle());
+        assertFalse(newBook4.isValidId(50));
 	}
+    	//Borrow/Return Validation
+	@Test
+	public void testBrrowReturn() throws Exception {
+		//Instantiate a Book object and a Member object.
+		Book newBook = new Book(100, "Programming");
+		Member newMember = new Member(1111,"George");
+		
+		//Use proper assertion to ensure that the book is available
+		assertTrue(newBook.isAvailable());
+		
+		//Retrieve Transaction instance
+		Transaction transaction = Transaction.getInstance();
+		
+		//call the borrowBook, borrow the book. Test borrowBook method and test book is not available after.
+		boolean borrowBook = transaction.borrowBook(newBook, newMember);
+		assertTrue(borrowBook);
+		assertFalse(newBook.isAvailable());
+		
+		//Borrow the book again
+		boolean borrowBookAgain = transaction.borrowBook(newBook, newMember);
+		assertFalse(borrowBookAgain);
+		
+		//Return the book, and verify if it's successful by check its availability
+		boolean returnBook = transaction.returnBook(newBook, newMember);
+		assertTrue(returnBook);
+
+
+		
+		//Return the book again.
+		boolean returnBookAgain = transaction.returnBook(newBook, newMember);
+		assertFalse(returnBookAgain);
+
+		
+	}
+
 
 }
